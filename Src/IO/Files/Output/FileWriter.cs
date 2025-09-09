f63af66751454a6033d99e5b464d9014ac4192c0
Baraa Abuhalima <B.K.1Baraa@gmail.com>
2025-09-09 21:35:56 +0300

    Add generic file input writer

---
+++ Src/IO/Files/Output/FileWriter.cs
@@ -0,0 +1,9 @@
+﻿namespace Real_time_weather_monitoring.IO.Files.Output;
+
+public class FileWriter: IFileWriter
+{
+    public void Write(string filePath, string content)
+    {
+        File.WriteAllText(filePath, content);
+    }
+}
