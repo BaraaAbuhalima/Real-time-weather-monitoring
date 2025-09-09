d5ff1e971f45ff9b8d4753aa112be39d0136542d
Baraa Abuhalima <B.K.1Baraa@gmail.com>
2025-09-10 00:48:59 +0300

    Add WeatherData class

---
+++ Src/Modles/WeatherData.cs
@@ -0,0 +1,17 @@
+﻿namespace Real_time_weather_monitoring.Modles;
+
+public class WeatherData
+{
+
+    public string Location { get; set; }
+    public double Temperature { get; set; }
+    public double Humidity { get; set; }
+    
+    public WeatherData(string location, double temperature, double humidity)
+    {
+        Location = location;
+        Temperature = temperature;
+        Humidity = humidity;
+    }
+    
+}
