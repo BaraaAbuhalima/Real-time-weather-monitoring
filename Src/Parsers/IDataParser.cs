c2dba7db91079707a3645b0bca75dd14a032adb8
Baraa Abuhalima <B.K.1Baraa@gmail.com>
2025-09-10 01:19:57 +0300

    Add data parser interface and implemented json parser

---
+++ Src/Parsers/IDataParser.cs
@@ -0,0 +1,8 @@
+﻿using Real_time_weather_monitoring.Modles;
+
+namespace Real_time_weather_monitoring.Parsers;
+
+public interface IDataParser<out T>
+{
+    public T Parse(string inputString);
+}
---
+++ Src/Parsers/JSONParser.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+
+namespace Real_time_weather_monitoring.Parsers;
+
+public class JSONParser< T>: IDataParser<T>
+{
+    public T Parse(string inputString)
+    {
+        return JsonSerializer.Deserialize<T>(inputString)!;
+    }
+}
