1141027d0a602aa0accc35a56dbe3e3dee9eb3f8
Baraa Abuhalima <B.K.1Baraa@gmail.com>
2025-09-10 02:57:52 +0300

    inject data parser factory with container and fix the typo Modles to Models

---
+++ Src/Containers/DataParserContainer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Real_time_weather_monitoring.Models;
+using Real_time_weather_monitoring.Parsers;
+
+namespace Real_time_weather_monitoring.Containers;
+
+public class DataParserContainer<T>
+{
+    private readonly ServiceProvider _serviceProvider=Initialize();
+
+    private static ServiceProvider Initialize()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(new XmlParser<T>());
+        services.AddSingleton(new JSONParser<T>());
+        return services.BuildServiceProvider();
+    } 
+    public TParser GetWeatherParser<TParser>() where TParser:IDataParser<T>
+    {
+        return _serviceProvider.GetService<TParser>()!;
+    }
+    
+}
+
